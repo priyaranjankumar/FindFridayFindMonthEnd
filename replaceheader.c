2ecfb289519eafe7b979b630f2ea8cfc28366dd9
Priya Ranjan Kumar <mishrasubham00@gmail.com>
2023-10-18 20:03:10 +0530

    updated the code to generate less output

--- replaceheader.c
+++ replaceheader.c
@@ -61,9 +61,9 @@ int main(int argc, char *argv[])
         printf("Month must be between 01 and 12");
         return 1;
     }
-    printf("argv[0]: %s\n", argv[0]);
-    printf("argv[1]: %s\n", argv[1]);
-    printf("argv[2]: %s\n", argv[2]);
+    // printf("argv[0]: %s\n", argv[0]);
+    // printf("argv[1]: %s\n", argv[1]);
+    // printf("argv[2]: %s\n", argv[2]);
     // Open the file in read-write mode
     FILE *file = fopen(argv[1], "r+b");
     if (file == NULL)
@@ -101,6 +101,9 @@ int main(int argc, char *argv[])
     if (pos != NULL)
     {
         strncpy(pos, new, strlen(new));
+    }
+    else {
+        printf("header not found, Skipping: %s\n", argv[1]")
     }
 
     // Write the modified header back to the file
